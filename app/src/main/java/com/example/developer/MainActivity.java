b435e0cf51978bb4efe197fc6c8a2b574a7c0f28
wangxiaosuo888888@icloud.com <wangxiaosuo888888@icloud.com>
2020-10-20 17:44:30 +0800

    add MotionLayout motionScene

--- app/src/main/java/com/example/developer/MainActivity.java
+++ app/src/main/java/com/example/developer/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.developer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        setContentView(R.layout.motion_layout);
 
     }
 
